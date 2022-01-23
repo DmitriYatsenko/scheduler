81652faf213a3bb69349326998d6b9efe19e6e79
Dmitri Yatsenko <dmitriyatsenko1995@gmail.com>
2022-01-23 01:55:20 +0000

    add <div> to Application.js

--- src/components/Application.js
+++ src/components/Application.js
@@ -7,18 +7,20 @@ export default function Application(props) {
     <main className="layout">
       <section className="sidebar">
         {
-          <img
-            className="sidebar--centered"
-            src="images/logo.png"
-            alt="Interview Scheduler"
-          />
-          <hr className="sidebar__separator sidebar--centered" />
-          <nav className="sidebar__menu"></nav>
-          <img
-            className="sidebar__lhl sidebar--centered"
-            src="images/lhl.png"
-            alt="Lighthouse Labs"
-          />
+          <div>
+            <img
+              className="sidebar--centered"
+              src="images/logo.png"
+              alt="Interview Scheduler"
+            />
+            <hr className="sidebar__separator sidebar--centered" />
+            <nav className="sidebar__menu"></nav>
+            <img
+              className="sidebar__lhl sidebar--centered"
+              src="images/lhl.png"
+              alt="Lighthouse Labs"
+            />
+          </div>
         }
       </section>
       <section className="schedule">
